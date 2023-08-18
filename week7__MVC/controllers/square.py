d5dd50b7b3c5a9d54ba729404b3e84ac6062841a
Neal <neal99185@gmail.com>
2023-08-18 21:34:47 +0800

    modify route path

--- week7__MVC/controllers/square.py
+++ week7__MVC/controllers/square.py
@@ -3,8 +3,7 @@ from flask import Blueprint, render_template
 square_controller = Blueprint("square", __name__)
 
 
-@square_controller.route("/<num>", methods=["POST"])
-def square(num):
-    number = int(num)
+@square_controller.route("/<int:number>", methods=["POST"])
+def square(number):
     answer = number * number
     return render_template("square.html", answer=answer)
